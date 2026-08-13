57beac3ca1c1d90c41c5c977591894107c0c631c
Aditya Raj Sharma <adityarajsharma8527784514@gmail.com>
2026-08-13 16:48:17 +0530

    Update website SEO and Flutter build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2592687201" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2798606471" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
