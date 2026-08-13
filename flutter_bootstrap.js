703db8a29aaae35cc0af78cba21f9c8b89948326
Aditya Raj Sharma <adityarajsharma8527784514@gmail.com>
2026-08-13 12:51:49 +0530

    Add Google Analytics

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "898794461" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4238604154" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
